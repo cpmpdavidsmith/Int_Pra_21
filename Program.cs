bfdf002f94b840007ef6c6481dc2ce049cca87db
David Smith <cpmpdavidsmith@icloud.com>
2024-01-13 15:18:18 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Queue ar = new Queue();
+
+            ar.Enqueue(1);
+            ar.Enqueue(2);
+            ar.Enqueue(3);
+
+            Console.WriteLine("There are " + ar.Count + " elememts in the queue.");
+            Console.Read();
+        }
+    }
+}
